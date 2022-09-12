8ad664a29a000762fcdffa6cc7eb65dcd4087542
Dylan Adams <113365542+max5max46@users.noreply.github.com>
2022-09-12 11:52:18 -0300

    Initial commit

---
+++ HelloWorld2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!, The Second Coming!!!");
+            Console.ReadKey(true);
+        }
+    }
+}
